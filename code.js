52ecd9faa1d1306b6b6eb65c7511891d6a8b9ea7
kenzieadmin <kenzieadmin@users.noreply.github.com>
2023-05-23 07:58:15 +0000

    deploy: 25bfc57e4e33defca604fb0ce36d4bc4aec011f1

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 // This function is provided to shake an image.  Use it every time the user clicks on a pokemon.
 // Feel free to delete this function and include it as a method within your classes...
-const toggleShake = function(element) {
+const toggleShake = function (element) {
   // Animations only occur when a new class is added to the element.
   // Since we want this to happen every time, we can switch between two different animations
   if (element.classList.contains("shake1")) {
@@ -15,3 +15,51 @@ const toggleShake = function(element) {
 const main = document.querySelector("main");
 
 // Your Code Here
+// let image = document.createElement('img')
+// image.src = 'images/pokeball.png'
+// image.setAttribute('style', 'width: 300px; height:300px')
+// toggleShake(image)
+// main.append(image)
+
+class Flowers {
+  constructor() {
+    this.image = document.createElement('img');
+    this.imagePaths = ['images/flower/flower0.png', 'images/flower/flower1.png', 'images/flower/flower2.png'];
+    this.index = 0;
+
+  }
+
+  growingFlowers() {
+    this.image.addEventListener('click', function(){
+    if(this.index < this.imagePaths.length-1){
+      this.index++
+      this.image.src = this.imagePaths[this.index]
+
+    } else if (this.index > this.imagePaths.length -1){
+      this.index = this.imagePaths.length -1
+      this.image.src = this.imagePaths[this.index]
+    }
+      console.log(this.index)
+
+    }.bind(this))
+    main.append(this.image)
+  }
+  toggleShake(element) {
+    // Animations only occur when a new class is added to the element.
+    // Since we want this to happen every time, we can switch between two different animations
+    if (element.classList.contains("shake1")) {
+      element.classList.remove("shake1");
+      element.classList.add("shake2")
+    } else {
+      element.classList.remove("shake2");
+      element.classList.add("shake1")
+    }
+  }
+  displaying() {
+    this.image.src = this.imagePaths[this.index]
+    main.append(this.image)
+  }
+}
+const CreateFlower = new Flowers()
+CreateFlower.displaying()
+CreateFlower.growingFlowers()
