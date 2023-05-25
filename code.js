16fd45bf22319bac00d7f62f7c97a80f8c5771cd
kenzieadmin <kenzieadmin@users.noreply.github.com>
2023-05-25 05:50:51 +0000

    deploy: ef291c3dd5c821a6d2b7c289a7cd330bab18754a

--- code.js
+++ code.js
@@ -21,6 +21,7 @@ class Flowers {
     this.image = document.createElement('img');
     this.imagePaths = ['images/flower/flower0.png', 'images/flower/flower1.png', 'images/flower/flower2.png'];
     this.index = 0;
+    this.lastTransformation = false;
     main.append(this.image)
 
   }
@@ -69,11 +70,11 @@ for (let i = 0; i < 20; i++) {
 
 //Pokemon class
 class Pokemon extends Flowers {
-  constructor() {
+  constructor(characterImages) {
     super()
     this.pokeImage = document.querySelector('img');
-    this.imagesContainer = ['images/pokeball.png', 'images/charmander/charmander0.png', 'images/charmander/charmander1.png', 'images/charmander/charmander2.png'];
-    this.pokeballBroken = 0;
+    this.imagesContainer = ['images/pokeball.png', ...characterImages];
+    this.nextTransformation = 0;
     this.ballAlreadyBroken = false;
     this.pokeImage.setAttribute('class', 'charmander');
 
@@ -83,9 +84,10 @@ class Pokemon extends Flowers {
     super.growingFlowers()
 
     this.pokeImage.addEventListener('click', function (event) {
-      this.pokeballBroken = Math.round(Math.random());
+      this.nextTransformation = Math.round(Math.random());
+      console.log(event.offsetTop)
 
-      if (this.pokeballBroken === 1 && (!this.ballAlreadyBroken || this.ballAlreadyBroken)) {
+      if (this.nextTransformation === 1 && (!this.ballAlreadyBroken || this.ballAlreadyBroken) && !this.lastTransformation) {
 
         if (this.index < this.imagesContainer.length - 1) {
           this.index++;
@@ -97,17 +99,18 @@ class Pokemon extends Flowers {
           this.index = this.imagesContainer.length - 1
           this.pokeImage = event.target
           this.pokeImage.src = this.imagesContainer[this.index]
+          this.lastTransformation = true
         }
         this.ballAlreadyBroken = true
-      } else if (this.pokeballBroken === 0 && this.ballAlreadyBroken === false) {
+      } else if (this.nextTransformation === 0 && this.ballAlreadyBroken === false && !this.lastTransformation) {
         this.pokeImage = event.target
         this.pokeImage.src = this.imagesContainer[this.index]
         this.toggleShake(this.pokeImage)
 
-      } else if (this.pokeballBroken === 0 && this.ballAlreadyBroken === true) {
+      } else if (this.nextTransformation === 0 && this.ballAlreadyBroken === true && !this.lastTransformation) {
 
         if (this.index < this.imagesContainer.length - 1) {
-          this.index++;
+
           this.pokeImage = event.target
           this.pokeImage.src = this.imagesContainer[this.index]
           this.toggleShake(this.pokeImage)
@@ -116,95 +119,95 @@ class Pokemon extends Flowers {
           this.index = this.imagesContainer.length - 1
           this.pokeImage = event.target
           this.pokeImage.src = this.imagesContainer[this.index]
+          this.lastTransformation = true
+
         }
       }
 
     }.bind(this));
   }
- 
+
   displayingPokeball() {
     super.displaying()
     this.pokeImage.src = this.imagesContainer[this.index]
   }
 }
 
-// charmander displaying
-const charmanaderDiv = document.createElement('div')
+
+// Pokemon displaying
+
+const charmanaderDiv = document.createElement('section')
 charmanaderDiv.setAttribute('class', 'charmanderContainer')
 document.body.append(charmanaderDiv)
 
-//some instance of charmanader  Pokemon
-//charmander1
-const Charmander1 = new Pokemon()
-Charmander1.displayingPokeball()
-Charmander1.growingPokemon()
+//some instance of Pokemon
+//charmander
+const charmanderImages = ['images/charmander/charmander0.png', 'images/charmander/charmander1.png', 'images/charmander/charmander2.png'];
+const Charmander = new Pokemon(charmanderImages)
+Charmander.displayingPokeball()
+Charmander.growingPokemon()
 
-//charmander2
-const Charmander2 = new Pokemon()
-Charmander2.displayingPokeball()
-Charmander2.growingPokemon()
+//pikachu
+const pikachuImages = ['images/pikachu/pikachu0.png', 'images/pikachu/pikachu1.png', 'images/pikachu/pikachu2.png', 'images/pikachu/pikachu3.png',]
+const pikachu = new Pokemon(pikachuImages)
+pikachu.displayingPokeball()
+pikachu.growingPokemon()
 
-//charmander3
-const Charmander3 = new Pokemon()
+//mankey
+const mankeyImages = ['images/mankey/mankey0.png', 'images/mankey/mankey1.png', 'images/mankey/mankey2.png']
+const Charmander3 = new Pokemon(mankeyImages)
 Charmander3.displayingPokeball()
 Charmander3.growingPokemon()
 
+//cyndaquil
+const cyndaquilImages = ['images/cyndaquil/quivala0.png', 'images/cyndaquil/quivala1.png', 'images/cyndaquil/quivala2.png', 'images/cyndaquil/quivala3.png']
+const Charmander4 = new Pokemon(cyndaquilImages)
+Charmander4.displayingPokeball()
+Charmander4.growingPokemon()
 
-
-
+// let eeveeCharacter = ['images/pokeball.png', 'images/charmander/charmander0.png', 'images/charmander/charmander1.png', 'images/charmander/charmander2.png'];
 
 //eevee displaying
-
-
 class EeveeClass extends Flowers {
   constructor() {
     super()
     this.eeveeImage = document.createElement('img')
     this.eeveeContainer = ['images/pokeball.png', 'images/eevee/eevee0.png', 'images/eevee/eevee1.png', 'images/eevee/eevee2.png', 'images/eevee/eevee3.png', 'images/eevee/eevee4.png', 'images/eevee/eevee5.png', 'images/eevee/eevee6.png', 'images/eevee/eevee7.png', 'images/eevee/eevee8.png']
-    this.pokeballBroken = 0;
+    this.transformation = 0;
+    this.nextTransformation = 0;
     this.ballAlreadyBroken = false;
     this.eeveeImage.setAttribute('class', 'eevee');
 
     eeveeDiv.append(this.eeveeImage)
   }
+
   growingEevee() {
     super.growingFlowers()
 
     this.eeveeImage.addEventListener('click', function (event) {
-      this.pokeballBroken = Math.round(Math.random());
+      this.transformation = Math.round(Math.random());
+      this.nextTransformation = Math.floor(Math.random()*8) + 2
+      // console.log(this.transformation)
+      
 
-      if (this.pokeballBroken === 1 && (!this.ballAlreadyBroken || this.ballAlreadyBroken)) {
+      if (this.transformation ===  1 && !this.ballAlreadyBroken && !this.lastTransformation ) {
 
-        if (this.index < this.eeveeContainer.length - 1) {
-          this.index++;
-          this.eeveeImage = event.target
-          this.eeveeImage.src = this.eeveeContainer[this.index]
-          this.toggleShake(this.eeveeImage)
-
-        } else if (this.index > this.eeveeContainer.length - 1) {
-          this.index = this.eeveeContainer.length - 1
-          this.eeveeImage = event.target
-          this.eeveeImage.src = this.eeveeContainer[this.index]
-        }
-        this.ballAlreadyBroken = true
-      } else if (this.pokeballBroken === 0 && this.ballAlreadyBroken === false) {
         this.eeveeImage = event.target
-        this.eeveeImage.src = this.eeveeContainer[this.index]
+        this.eeveeImage.src = this.eeveeContainer[this.transformation]
         this.toggleShake(this.eeveeImage)
 
-      } else if (this.pokeballBroken === 0 && this.ballAlreadyBroken === true) {
+        this.ballAlreadyBroken = true
+      } else if (this.transformation === 1 && this.ballAlreadyBroken && !this.lastTransformation) {
+        this.eeveeImage = event.target
+        this.eeveeImage.src = this.eeveeContainer[this.nextTransformation]
+        this.toggleShake(this.eeveeImage)
 
-        if (this.index < this.eeveeContainer.length - 1) {
-          this.index++;
-          this.eeveeImage = event.target
-          this.eeveeImage.src = this.eeveeContainer[this.index]
-          this.toggleShake(this.eeveeImage)
+        this.lastTransformation = true;
+       
 
-        } else if (this.index > this.eeveeContainer.length - 1) {
-          this.index = this.eeveeContainer.length - 1
-          this.eeveeImage = event.target
-          this.eeveeImage.src = this.eeveeContainer[this.index]
-        }
+      } else if (this.transformation === 0 && (!this.ballAlreadyBroken || this.ballAlreadyBroken) && !this.lastTransformation){
+        this.eeveeImage = event.target
+        this.toggleShake(this.eeveeImage)
       }
 
     }.bind(this));
@@ -216,87 +219,31 @@ class EeveeClass extends Flowers {
   }
 }
 
-const eeveeDiv = document.createElement('div')
+
+// eevee displaying
+const eeveeDiv = document.createElement('section')
 eeveeDiv.setAttribute('class', 'eeveeDiv')
 document.body.append(eeveeDiv)
 
-
+//some instance of eevee  Pokemon
+//eevee1
 const eevee1 = new EeveeClass()
 eevee1.displayingPokeball()
 eevee1.growingEevee()
 
+//eevee2
 const eevee2 = new EeveeClass()
 eevee2.displayingPokeball()
 eevee2.growingEevee()
 
+//eevee3
 const eevee3 = new EeveeClass()
 eevee3.displayingPokeball()
 eevee3.growingEevee()
-// let eeveeGroup = document.createElement('div')
-// eeveeGroup.setAttribute('class', 'eeveeGroup')
 
-// class EeveeClass extends Pokemon {
-//   constructor() {
-//     super()
-//     this.randomIndex = Math.round(Math.random())
-//     this.eeveeImage = document.createElement('img')
-//     this.eeveeContainer = ['images/pokeball.png', 'images/eevee/eevee0.png', 'images/eevee/eevee1.png', 'images/eevee/eevee2.png', 'images/eevee/eevee3.png', 'images/eevee/eevee4.png', 'images/eevee/eevee5.png', 'images/eevee/eevee6.png', 'images/eevee/eevee7.png', 'images/eevee/eevee8.png']
-
-//   }
-//   displayInitialForm = () => {
-//     // super.displayingPokeball()
-//     this.eeveeImage.src = this.imagesContainer[this.index]
-//   }
-
-//   changeForm() {
-//     // super.growingPokemon()
-//     this.eeveeImage.addEventListener('click', function (event) {
-//       this.pokeballBroken = Math.round(Math.random());
-
-//       if (this.pokeballBroken === 1 && (!this.ballAlreadyBroken || this.ballAlreadyBroken)) {
-
-//         if (this.index < this.imagesContainer.length - 1) {
-//           this.index++;
-//           this.eeveeImage = event.target
-//           this.eeveeImage.src = this.imagesContainer[this.index]
-//         } else if (this.index > this.imagesContainer.length - 1) {
-//           this.index = this.imagesContainer.length - 1
-//           this.eeveeImage = event.target
-//           this.eeveeImage.src = this.imagesContainer[this.index]
-//         }
-//         this.ballAlreadyBroken = true
-//       } else if (this.pokeballBroken === 0 && this.ballAlreadyBroken === false) {
-//         this.eeveeImage = event.target
-//         this.eeveeImage.src = this.imagesContainer[this.index]
-//         this.toggleShake(this.eeveeImage)
-
-//       } else if (this.pokeballBroken === 0 && this.ballAlreadyBroken === true) {
-
-//         if (this.index < this.imagesContainer.length - 1) {
-//           this.index++;
-//           this.eeveeImage = event.target
-//           this.eeveeImage.src = this.imagesContainer[this.index]
-//         } else if (this.index > this.imagesContainer.length - 1) {
-//           this.index = this.imagesContainer.length - 1
-//           this.eeveeImage = event.target
-//           this.eeveeImage.src = this.imagesContainer[this.index]
-//         }
-//       }
-
-//     }.bind(this));
-//   }
-// }
-
-
-// const EeveeInstance = new EeveeClass()
-// EeveeInstance.displayInitialForm()
-// EeveeInstance.changeForm()
-// EeveeInstance.eeveeImage.setAttribute('class', 'eeveeForms')
-
-// EeveeInstance.eeveeImage.setAttribute('class', 'eeveeForms')
-
-// eeveeGroup.append(EeveeInstance.eeveeImage)
-// document.body.append(eeveeGroup)
-
-// console.log(Pokemon instanceof Flowers)
+//eevee4
+const eevee4 = new EeveeClass()
+eevee4.displayingPokeball()
+eevee4.growingEevee()
+ 
 
